a258545bafb11f4fb0b3e9e7fe41a65fcec83b2f
norie <chengjy1991@gmail.com>
2020-12-05 16:52:21 -0500

    GAME2001Lab5Part2
    -add doubleendedlinkedlist.h and test file for it

---
+++ Week05/DoubleEndedLinkListTest.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include "DoubleEndedLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Double-Ended Linked list Example" << endl << endl;
+
+	LinkedList<int> lList;
+
+	lList.Push(101);
+	lList.Push(11);
+	lList.Pop();
+	lList.Push(201);
+	lList.Push(301);
+	lList.Push(401);
+	lList.Push(1);
+	lList.Pop();
+	lList.Push(701);
+	lList.Push_Front(11);
+
+	LinkIterator<int> it;
+
+	for (it = lList.Begin(); it != lList.End(); it++)
+	{
+		cout << " " << *it;
+	}
+
+	cout << endl << endl;
+
+	return 0;
+}
